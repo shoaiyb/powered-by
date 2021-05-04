0782cb03e7e078bb1435d26cc159ae5ce6c8331f
shoaiyb sysa <75518689+shoaiyb@users.noreply.github.com>
2021-05-04 15:29:36 +0100

    Create PoweredBy.php

---
+++ PoweredBy.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * Powered By - WonderCMS Plugin
+ *
+ * The plugin add (...)
+ *
+ * @package PoweredBy
+ * @author shoaiyb sysa <https://sysa.ml>
+ * @require WonderCMS v3.*.*
+ * @version 1.0
+ * 
+*/
+
+function poweredBy() {
+<div style="text-align: right;position: fixed;z-index:9999999;bottom: 0;width: auto;right: 1%;cursor: pointer;line-height: 0;display:block !important;">
+<a title="Powered by WonderCMS." target="_blank" href="https://www.wondercms.com/?utm_source=siteTitle&utm_medium=banner&utm_campaign=powered_by_logo">
+<img src="plugins/PoweredBy/poweredby.png" alt="Powered by WonderCMS">
+</a>
+</div>
+}
