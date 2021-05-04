bdfca9faa7eb461f09910b89aa67f3fbd2ea24d2
shoaiyb sysa <75518689+shoaiyb@users.noreply.github.com>
2021-05-04 23:24:57 +0100

    Update PoweredBy.php

--- PoweredBy.php
+++ PoweredBy.php
@@ -20,7 +20,7 @@ function poweredBy($args) {
   $args[0] .= <<<HTML
                  <div style="text-align: right;position: fixed;z-index:9999999;bottom: 0;width: auto;right: 1%;cursor: pointer;line-height: 0;display:block !important;">
                      <a title="Powered by WonderCMS." target="_blank" href="https://www.wondercms.com/?utm_source=$Wcms->get('config', 'siteTitle')&utm_medium=banner&utm_campaign=powered_by_logo">
-                         <img src="plugins/PoweredBy/poweredby.png" alt="Powered by WonderCMS">
+                         <img src="$this->url()plugins/PoweredBy/poweredby.png" alt="Powered by WonderCMS">
                      </a>
                  </div>
 HTML;
