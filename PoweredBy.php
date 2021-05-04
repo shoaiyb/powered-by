071a01247f4cbc91f30f06dc9b2d0d94e1b3f1b9
shoaiyb sysa <75518689+shoaiyb@users.noreply.github.com>
2021-05-04 17:40:10 +0100

    Update PoweredBy.php

--- PoweredBy.php
+++ PoweredBy.php
@@ -5,10 +5,10 @@
  * The plugin add (...)
  *
  * @package PoweredBy
+ * @version 1.0
  * @author shoaiyb sysa <https://sysa.ml>
  * @require WonderCMS v3.*.*
- * @version 1.0
- * 
+ * @tested WonderCMS v3.1.4
 */
 
 
@@ -19,7 +19,7 @@ function poweredBy($args) {
     if($Wcms->loggedIn) return $args;
   $args[0] .= <<<HTML
                  <div style="text-align: right;position: fixed;z-index:9999999;bottom: 0;width: auto;right: 1%;cursor: pointer;line-height: 0;display:block !important;">
-                     <a title="Powered by WonderCMS." target="_blank" href="https://www.wondercms.com/?utm_source=siteTitle&utm_medium=banner&utm_campaign=powered_by_logo">
+                     <a title="Powered by WonderCMS." target="_blank" href="https://www.wondercms.com/?utm_source=$Wcms->get('config', 'siteTitle')&utm_medium=banner&utm_campaign=powered_by_logo">
                          <img src="plugins/PoweredBy/poweredby.png" alt="Powered by WonderCMS">
                      </a>
                  </div>
