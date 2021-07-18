7edc7cb6d17b9c46c57eff690ea954c879dfc1f2
shoaiyb sysa <75518689+shoaiyb@users.noreply.github.com>
2021-07-18 21:46:06 +0100

    Update PoweredBy.php

--- PoweredBy.php
+++ PoweredBy.php
@@ -7,19 +7,19 @@
  * @package PoweredBy
  * @version 1.0
  * @author shoaiyb sysa <https://sysa.ml>
- * @require WonderCMS v3.*.*
+ * @require WonderCMS v3.*
  * @tested WonderCMS v3.1.4
 */
 
 
 global $Wcms;
   
-function poweredBy($args) {
+function PoweredBy($args) {
     global $Wcms;
     if($Wcms->loggedIn) return $args;
   $args[0] .= <<<HTML
                  <div style="text-align: right;position: fixed;z-index:9999999;bottom: 0;width: auto;right: 1%;cursor: pointer;line-height: 0;display:block !important;">
-                     <a title="Powered by WonderCMS." target="_blank" href="https://www.wondercms.com/?utm_source={$Wcms->get('config', 'siteTitle')}&utm_medium=banner&utm_campaign=powered_by_logo">
+                     <a title="Powered by WonderCMS." target="_blank" href="https://www.wondercms.com/?utm_source={$Wcms->get('config', 'siteTitle')}&utm_medium=banner&utm_campaign=PoweredBy">
                          <img src="{$Wcms->url('plugins/PoweredBy/poweredby.png')}" alt="Powered by WonderCMS">
                      </a>
                  </div>
@@ -27,5 +27,5 @@ HTML;
   return $args;
   }
 
-$Wcms->addListener("js", "poweredBy");
+$Wcms->addListener("js", "PoweredBy");
 ?>
