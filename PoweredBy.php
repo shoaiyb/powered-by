60a65ba2cad7234d46c1953be4f6ba548f1bc5cc
Shoaiyb Sysa <75518689+shoaiyb@users.noreply.github.com>
2022-03-28 15:20:21 +0100

    Update PoweredBy.php

--- PoweredBy.php
+++ PoweredBy.php
@@ -11,20 +11,35 @@
  * @license MIT
 */
 global $Wcms;
+
+function powered_by_header() {
+  global $Wcms;
+  if(!headers_sent()) {
+    header('X-Powered-By: WonderCMS ' . $Wcms->version);
+  }
+}
   
-function PoweredBy($args) {
+function powered_by_meta() {
+  global $Wcms;
+  return '<meta name="generator" content="WonderCMS ' . $Wcms->version . '">';
+}
+
+function powered_by_image($args) {
   global $Wcms;
   if($Wcms->loggedIn) {
     return $args;
   }
-  $args[0] .= <<<HTML
-              <div style="text-align: right;position: fixed;z-index:9999999;bottom: 0;width: auto;right: 1%;cursor: pointer;line-height: 0;display:block !important;">
-                <a title="Powered by WonderCMS" target="_blank" rel="nofollow" href="https://www.wondercms.com/?utm_source={$Wcms->get('config', 'siteTitle')}&utm_medium=banner&utm_campaign=PoweredBy">
-                  <img src="{$Wcms->url('plugins/PoweredBy/poweredby.png')}" alt="Powered by WonderCMS">
-                </a>
-              </div>
-              HTML;
+  $args[0] .= '
+  <div style="text-align: right;position: fixed;z-index:9999999;bottom: 0;width: auto;right: 1%;cursor: pointer;line-height: 0;display:block !important;">
+    <a title="Powered by WonderCMS" target="_blank" rel="nofollow" href="https://www.wondercms.com/?utm_source=' . $Wcms->get('config', 'siteTitle') . '">
+      <img src="' . $Wcms->url('plugins/PoweredBy/poweredby.png') . '" alt="Powered by WonderCMS">
+    </a>
+  </div>
+  ';
   return $args;
 }
-$Wcms->addListener("js", "PoweredBy");
+
+powered_by_header();
+$Wcms->addListener("css", "powered_by_meta");
+$Wcms->addListener("js", "powered_by_image");
 ?>
